43477d1759a3b4988bc60e2a34fce1f593817c92
Sheeno <sinoglmatej@gmail.com>
2020-12-14 18:58:46 +0100

    lvl3

--- levels.py
+++ levels.py
@@ -14,41 +14,101 @@ test.new_spawner(600, 200, 150, 30, (255, 255, 0))
 test.new_spawner(700, 500, 90, 40, (255, 0, 255))
 
 # LVL 2
-lvl2 = Level(size = (1382, 778))
+lvl2 = Level(size=(1382, 778))
 
-# Spawner 1
+# SPAWNERS
+# 1
 lvl2.new_spawner(600, 300, 100, 30, (0, 0, 255))
 
-# Spawner 2
+# 2
 lvl2.new_spawner(800, 300, 100, 30, (0, 255, 0))
 
-# Spawner 3
+# 3
 lvl2.new_spawner(100, 700, 100, 30, (255, 255, 0))
 
-# Spawner 4
+# 4
 lvl2.new_spawner(1282, 700, 100, 30, (255, 0, 0))
 
 
-# Block 1
+# BLOCKS
+# 1
 lvl2.new_block(500, 250, 50, 150)
 
-# Block 2
+# 2
 lvl2.new_block(900, 250, 50, 150)
 
-# Block 3
+# 3
 lvl2.new_block(700, 0, 50, 650)
 
-# Block 4
+# 4
 lvl2.new_block(500, 400, 450, 50)
 
-# Block 5
+# 5
 lvl2.new_block(0, 550, 300, 50)
 
-# Block 6
+# 6
 lvl2.new_block(1082, 550, 300, 50)
 
-# Block 7
+# 7
 lvl2.new_block(500, 628, 50, 150)
 
-# Block 8
+# 8
 lvl2.new_block(900, 628, 50, 150)
+
+
+# LVL 3
+lvl3 = Level(size=(1382, 778))
+
+# SPAWNERS
+# 1
+lvl3.new_spawner(150, 678, 100, 30, (255, 0, 0))
+
+# 2
+lvl3.new_spawner(541, 678, 100, 30, (0, 255, 0))
+
+# 3
+lvl3.new_spawner(882, 678, 100, 30, (0, 0, 255))
+
+# 4
+lvl3.new_spawner(1232, 678, 100, 30, (255, 255, 0))
+
+
+# BLOCKS
+# 1
+lvl3.new_block(0, 350, 250, 50)
+
+# 2
+lvl3.new_block(200, 600, 200, 50)
+
+# 3
+lvl3.new_block(250, 200, 150, 50)
+
+# 4
+lvl3.new_block(400, 200, 50, 578)
+
+# 5
+lvl3.new_block(450, 575, 125, 50)
+
+# 6
+lvl3.new_block(575, 225, 282, 100)
+
+# 7
+lvl3.new_block(691, 0, 50, 100)
+
+# 8
+lvl3.new_block(691, 500, 50, 278)
+
+# 9
+lvl3.new_block(857, 575, 150, 50)
+
+# 10
+lvl3.new_block(982, 200, 50, 578)
+
+# 11
+lvl3.new_block(1032, 200, 150, 50)
+
+# 12
+lvl3.new_block(1179, 350, 250, 50)
+
+# 13
+lvl3.new_block(1032, 600, 200, 50)
